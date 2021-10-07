f5a016bfb00998e4c99d9b7267a3b84553987617
MWalkerSr <mike@fsmail.bradley.edu>
2021-10-07 16:16:07 -0500

    js scripts

---
+++ Web_Dev/myScript.js
@@ -0,0 +1,6 @@
+/* function myfun(){
+    document.getElementById('pid').innerHTML='Content is replaced by JavaScript';
+} */
+function resetmyfun(){
+    document.getElementById('pid').innerHTML='Content from HTML';
+}
